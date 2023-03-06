39c4de609b4790a5405ea2a9d96bbb7b8c5c8a20
patrickluizdev <patrickluizjf@hotmail.com>
2023-03-05 22:14:56 -0300

    add component Tweet

---
+++ twitter-react/src/components/Tweet.tsx
@@ -0,0 +1,5 @@
+export function Tweet(){
+  return(
+    <div> Teste</div>
+  )
+}
--- twitter-react/src/main.tsx
+++ twitter-react/src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Tweet } from './components/Tweet'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <div>
+    <Tweet/>
+    </div>
 
   </React.StrictMode>,
 )

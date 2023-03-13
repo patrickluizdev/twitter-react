9a2488b8c414fe9d17f0e6236ab28b62d7e63260
patrickluizdev <patrickluizjf@hotmail.com>
2023-03-13 10:02:38 -0300

    style

--- twitter-react/src/components/Tweet.tsx
+++ twitter-react/src/components/Tweet.tsx
@@ -1,11 +1,26 @@
-export function Tweet(props){
+interface TweetProps{
+  user: string
+  children: string
+  likes?: number
+}
+const tweetStyles = {
+  borderRadius: 10 
+}
+
+export function Tweet(props: TweetProps){
   return(
-    <div> 
-      <strong>
-      {props.user}
-      </strong>
-      <a>  {props.content}  </a>
-      <button>Like</button>
+    <div className="tweet" style={tweetStyles}> 
+      <p> 
+        <p>
+            <strong>
+              {props.user}
+            </strong>    
+        </p>
+        <p>
+          {props.children}   
+        </p> 
+        <button>Like {props.likes ?? 0}</button>
+      </p>
     </div>
   )
 }
--- twitter-react/src/main.tsx
+++ twitter-react/src/main.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Tweet } from './components/Tweet'
+import './add/global.css';
+import twitterLogo from './assets/logo-twitter.svg';
+import { House } from 'phosphor-react';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>
-    <Tweet user='Patrick' content='Meu Primeiro Tweet' />
-    
-    <Tweet user='React' content='Hello World' />
+    <div className='layout'>
+      <aside className='side-bar'>
+        <img src={twitterLogo} alt="logo" className='logo'/>
+        <nav className='main-navigation'>
+          <a href="#">
+            <House/>
+            Home
+          </a>
+          <a href="#">Explorer</a>
+          <a href="#">Notification</a>
+          <a href="#">Messages</a>
+          <a href="#">Bookmarks</a>
+          <a href="#">List</a>
+          <a href="#">Profile</a>
+          <a href="#">More</a>
+        </nav>
+        <button className='new-tweet' type='button'>Tweet</button>  
+      </aside>
+      <div className="content">
+        content
+      </div>
     </div>
-
   </React.StrictMode>,
 )

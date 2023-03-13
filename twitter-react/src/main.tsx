cd8acf5367d46884941fb851831862db5fefeaba
patrickluizdev <patrickluizjf@hotmail.com>
2023-03-13 16:03:40 -0300

    main

--- twitter-react/src/main.tsx
+++ twitter-react/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './add/global.css';
 import twitterLogo from './assets/logo-twitter.svg';
-import { House } from 'phosphor-react';
+import { Bell, Bookmark, DotsThree, DotsThreeCircle, DotsThreeVertical, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -11,22 +11,55 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <aside className='side-bar'>
         <img src={twitterLogo} alt="logo" className='logo'/>
         <nav className='main-navigation'>
-          <a href="#">
-            <House/>
+          <a className='active' href="#">
+            <House weight='fill'/>
             Home
           </a>
-          <a href="#">Explorer</a>
-          <a href="#">Notification</a>
-          <a href="#">Messages</a>
-          <a href="#">Bookmarks</a>
-          <a href="#">List</a>
-          <a href="#">Profile</a>
-          <a href="#">More</a>
+          <a href="#">
+            <Hash/>
+            Explorer
+          </a>
+          <a href="#">
+            <Bell/>
+            Notification
+          </a>
+          <a href="#">
+            <Envelope/>
+            Messages
+          </a>
+          <a href="#">
+            <Bookmark/>
+            Bookmarks
+          </a>
+          <a href="#">
+            <FileText/>
+            List
+          </a>
+          <a href="#">
+            <User/>
+            Profile
+          </a>
+          <a href="#">
+            <DotsThreeCircle/>
+            More
+          </a>
         </nav>
         <button className='new-tweet' type='button'>Tweet</button>  
       </aside>
       <div className="content">
-        content
+        <main className="time-line">
+          <div className='time-line-header'>
+            Home
+            <Sparkle/>
+          </div>
+
+          <form className='new-tweet-form' action="">
+            <label htmlFor="tweet">
+              <img src="https://github.com/patrickluizdev.png" alt="Patrick Luiz" />
+              <textarea name="" id="tweet"/>
+            </label>
+          </form>
+        </main>
       </div>
     </div>
   </React.StrictMode>,
